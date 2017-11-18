8648978af8d92afb145074bfd60f7bd17b91d469
yorche <hyaoki123@gmail.com>
2017-11-18 00:59:52 -0600

    tratando string (c,c++,go)

---
+++ perl/helloconsole/hello.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+
+print "¿Cuál es tu nombre?:   ";
+$name = <>;
+print "Hola   $name";
+
+exit;
---
+++ ruby/helloconsole/hello.rb
@@ -0,0 +1,4 @@
+# coding: utf-8
+puts "¿Cuál es tu nombre?   "
+name = gets
+puts "Hola  "+ name
