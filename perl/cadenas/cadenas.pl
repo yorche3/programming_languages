39503a2b5aaa41105826cd6f6fd2ed3088b1c397
yorche <hyaoki123@gmail.com>
2017-11-21 11:57:54 -0600

    cadenas en perl, go, ruby ...

--- perl/cadenas/cadenas.pl
+++ perl/cadenas/cadenas.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/perl
 
 =begin comment
--regresa una cadena con un saludo
+Genera una cadena con un saludo
+
+@return una cadena
 =end comment
 =cut
 sub Hola{
@@ -9,20 +11,62 @@ sub Hola{
 }
 
 =begin comment
+Verifica si una cadena es palindromo
+
+@param _[0] : cadena
+
+@return 1 si lo es, 0 en otro caso
 =end comment
 =cut
 sub Palindromo{
+    my @arr = split //, $_[0];
     $i = 0;
-    $f = length $_[0] - 1;
-    if(substr($_[0], $i, $i) ne substr($_[0], $f, $f)){
-	return 0;
+    $f = length $_[0];
+    $f--;
+    while($i < $f){
+	$char1 = $arr[$i];
+        $char2 = $arr[$f];
+	if($char1 ne $char2){
+	    return 0;
+	}
+	$i++;
+	$f--;
     }
     return 1;
+}
+
+=begin comment
+Cuenta el número de repeticiones de un caracter en una cadena
+
+@param $_[0] : caracter
+@param $_[1] : cadena
+
+@return c un entero con el conteo
+= end comment
+=cut
+sub Repeticiones{
+    my @arr = split //, $_[1];
+    $i = 0;
+    $l = length $_[1];
+    $c = 0;
+    my $char = substr($_[0],0,1);
+    while($i < $l){
+	$c_char = $arr[$i];
+	if($char eq $c_char){
+	    $c++;
+	}
+	$i++;
+    }
+    return $c;
 }
 
 $saludo = Hola();
 print "$saludo\n";
 print "Escribe una palabra   ";
-$word = <>;
+chomp ($word = <>);
 $es_palindromo = Palindromo($word);
 print "Es palindromo  $es_palindromo\n";
+print "Caracter a buscar :   ";
+$char = <>;
+$num_repeticiones = Repeticiones($char, $word);
+print "repeticiones : $num_repeticiones\n";
