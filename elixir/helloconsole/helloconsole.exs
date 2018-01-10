259499db59d2d6bd90edf9699cae01291481c9cf
yorche <hyaoki123@gmail.com>
2018-01-09 19:37:49 -0600

    hello console elixir

---
+++ elixir/helloconsole/helloconsole.exs
@@ -0,0 +1,2 @@
+name = IO.gets "¿Cuál es tu nombre?   "
+IO.puts "Hello " <> name
---
+++ ocaml/helloconsole/helloconsole.ml
@@ -0,0 +1,3 @@
+let () = print_string "¿Cual es tu nombre?   "
+let name = read_line ()
+let () = Printf.printf "Hola %s\n" name
