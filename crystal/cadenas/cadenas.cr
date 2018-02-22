af0c43be55ebd91dc003c71de34112f2a5a63c9b
yorche3 <hyaoki123@gmail.com>
2018-02-22 17:19:30 -0600

    crystal tratar cadenas

--- crystal/cadenas/cadenas.cr
+++ crystal/cadenas/cadenas.cr
@@ -1,7 +1,12 @@
+# Returns un saludo
 def hola
   return "Hola"
 end
 
+# Verifica si una palabra es palindromo
+#
+# param *word*  palabra a evaluar
+# returns  true si lo es false en otro caso
 def palindromo(word)
   i = 0
   l = word.size - 1
@@ -15,7 +20,35 @@ def palindromo(word)
   return true
 end
 
+# Cuenta el número de repeticiones de un carácter en una palabra
+#
+# param *word*  palabra a iterar
+# param *ch*    carácter a buscar
+# returns el número de apariciones
+def repeticiones(word, ch)
+  i = 0
+  l = word.size
+  c = 0
+  while(i < l)
+    if word[i] == ch
+      c += 1
+    end
+    i += 1
+  end
+  return c
+end
+
+# Invierte una cadena
+def reversa(word)
+  return word.reverse
+end
+
 puts hola()
 puts "Escribe una palabra"
 word = read_line
 puts "Es palindromo : #{palindromo(word)}"
+puts "Carácter a buscar"
+car = read_line
+ch = car[0]
+puts "repeticiones : #{repeticiones(word,ch)}"
+puts "palabra invertida   #{reversa(word)}"
